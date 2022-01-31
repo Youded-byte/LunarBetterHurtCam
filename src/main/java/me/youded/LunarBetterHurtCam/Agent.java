68af6c6280e9fae709485742ef03b338b0ea0725
Youded-byte <82912625+Youded-byte@users.noreply.github.com>
2022-01-31 01:47:20 +0100

    init

---
+++ src/main/java/me/youded/LunarBetterHurtCam/Agent.java
@@ -0,0 +1,9 @@
+package me.youded.LunarBetterHurtCam;
+
+import java.lang.instrument.Instrumentation;
+
+public class Agent {
+    public static void premain(String args, Instrumentation inst){
+        inst.addTransformer(new Transformer(args), true);
+    }
+}
