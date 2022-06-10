e5fbc972c70969909aa4e145e2c2e16718dcb10a
Youded-byte <82912625+Youded-byte@users.noreply.github.com>
2022-06-10 16:52:04 +0200

    Add Support for Newer Minecraft Versions

--- src/main/java/me/youded/LunarBetterHurtCam/Agent.java
+++ src/main/java/me/youded/LunarBetterHurtCam/Agent.java
@@ -4,6 +4,6 @@ import java.lang.instrument.Instrumentation;
 
 public class Agent {
     public static void premain(String args, Instrumentation inst){
-        inst.addTransformer(new Transformer(args), true);
+        inst.addTransformer(new Transformer(args));
     }
 }
--- src/main/java/me/youded/LunarBetterHurtCam/Transformer.java
+++ src/main/java/me/youded/LunarBetterHurtCam/Transformer.java
@@ -12,59 +12,53 @@ import java.security.ProtectionDomain;
 
 public class Transformer implements ClassFileTransformer {
     private Float hurtfloat;
-    private boolean found;
 
-    
-    Transformer(String args){
-        if(args != null && !args.isBlank() && Float.parseFloat(args) > -1)
+    Transformer(String args) {
+        if (args != null && !args.isBlank() && Float.parseFloat(args) > -1)
             this.hurtfloat = Float.parseFloat(args);
-        else if(System.getProperty("hurtanimationmodifier") != null && !System.getProperty("hurtanimationmodifier").isBlank())
+        else if (System.getProperty("hurtanimationmodifier") != null
+                && !System.getProperty("hurtanimationmodifier").isBlank())
             this.hurtfloat = Float.parseFloat(System.getProperty("hurtanimationmodifier"));
         else
             this.hurtfloat = 6.0F;
-            
     }
 
     @Override
-    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
+            ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
         if (classfileBuffer == null || classfileBuffer.length == 0) {
             return new byte[0];
         }
-
-        if(!className.startsWith("net/minecraft")) {
+        if (!className.startsWith("net/minecraft")) {
             return classfileBuffer;
         }
 
         ClassReader cr = new ClassReader(classfileBuffer);
-        if(!this.found && cr.getInterfaces().length == 2 && "java/lang/Object".equals(cr.getSuperName())) {
+        if (cr.getAccess() == Opcodes.ACC_SUPER + Opcodes.ACC_PUBLIC) {
             ClassNode cn = new ClassNode();
 
             cr.accept(cn, 0);
 
             for (MethodNode method : cn.methods) {
-                if ("(F)V".equals(method.desc) && method.access == 1
-                && method.instructions.get(0).getOpcode() == -1
-                && method.instructions.get(1).getOpcode() == -1
-                && method.instructions.get(2).getOpcode() == 25
-                && method.instructions.get(3).getOpcode() == 180
-                && method.instructions.get(4).getOpcode() == 182
-                && method.instructions.get(5).getOpcode() == 193
-                && method.instructions.get(6).getOpcode() == 153
-                && method.instructions.get(7).getOpcode() == -1
-                && method.instructions.get(8).getOpcode() == -1
-                && method.instructions.get(9).getOpcode() == 25
-                && method.instructions.get(10).getOpcode() == 180
-                && method.instructions.get(11).getOpcode() == 182
-                && method.instructions.get(12).getOpcode() == 192
-                && method.instructions.get(13).getOpcode() == 58
-                ) {
-                    for(AbstractInsnNode insn : method.instructions) {
-                        if(insn.getOpcode() == Opcodes.LDC && (Float)((LdcInsnNode)insn).cst == 14.0F) {
-                            this.found = true;
-                            method.instructions.set(insn, new LdcInsnNode(this.hurtfloat));
-                            ClassWriter cw = new ClassWriter(cr, 0);
-                            cn.accept(cw);
-                            return cw.toByteArray();
+                if (method.desc.endsWith("F)V")) {
+                    int amountofeightthousand = 0;
+                    int amountoffourteen = 0;
+                    for (AbstractInsnNode insn : method.instructions) {
+                        if (insn.getOpcode() == Opcodes.LDC && ((LdcInsnNode) insn).cst.equals(14.0F)) {
+                            amountoffourteen++;
+                        }
+                        if (insn.getOpcode() == Opcodes.LDC && ((LdcInsnNode) insn).cst.equals(8000.0F)) {
+                            amountofeightthousand++;
+                        }
+                    }
+                    if (amountoffourteen == 1 && amountofeightthousand == 1) {
+                        for (AbstractInsnNode insn : method.instructions) {
+                            if (insn.getOpcode() == Opcodes.LDC && ((LdcInsnNode) insn).cst.equals(14.0F)) {
+                                method.instructions.set(insn, new LdcInsnNode(this.hurtfloat));
+                                ClassWriter cw = new ClassWriter(cr, 0);
+                                cn.accept(cw);
+                                return cw.toByteArray();
+                            }
                         }
                     }
                 }
